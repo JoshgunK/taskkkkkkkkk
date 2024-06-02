def257b3a5344a4f5ca54eeeea321485e7c73922
Joshgun <169159204+JoshgunK@users.noreply.github.com>
2024-06-02 23:05:11 +0400

    Task

---
+++ hometask2/hometask2/Car.cs
@@ -0,0 +1,22 @@
+﻿public class Car
+{
+    private static int _idCounter = 1;
+
+    public int Id { get; }
+    public string Brand { get; }
+    public string Model { get; }
+    public CarType Type { get; }
+
+    public Car(string brand, string model, CarType type)
+    {
+        Id = _idCounter++;
+        Brand = brand;
+        Model = model;
+        Type = type;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Brand: {Brand}, Model: {Model}, Type: {Type}";
+    }
+}
---
+++ hometask2/hometask2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public enum CarType
+{
+    Sport,
+    SUV,
+    Sedan
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car1 = new Car("Mercedes", "Cls550", CarType.Sedan);
+        Car car2 = new Car("BMW", "F10", CarType.Sedan);
+        Car car3 = new Car("Volvo", "X90", CarType.SUV);
+
+        Console.WriteLine(car1);
+        Console.WriteLine(car2);
+        Console.WriteLine(car3);
+    }
+}
